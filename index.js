f7a02712b039e89e5e5d8659e39f06b1c605ff8f
KyllianGamer <kylleke@telenet.be>
2020-07-10 17:04:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function onMessageHandler (target, context, msg, self) {
   const commandName = msg.trim();
   if (commandName === "!skip") {
     if (access_token) {
-      const data = await fetch.get('http://streamlinkpixel.herokuapp.com/mods-api/channels/PixelPAVL').toJSON;
+      const data = await fetch.get('http://streamlinkpixel.herokuapp.com/mods-api/channels/PixelPAVL').toJSON();
       console.log(data);
       if (!data.moderators.includes(target.substr(1)) && target.substr(1) != 'pixelpavl') return;
       SkipSong();
